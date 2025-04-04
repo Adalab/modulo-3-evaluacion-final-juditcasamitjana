8d7a405f0124a2674a9329520a4b71050cc5f500
Judit Casamitjana <juditcasamitjana@gmail.com>
2025-04-04 10:51:34 +0200

    add router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,27 @@
+import { Route, Routes } from "react-router-dom";
 import "../scss/App.scss";
+import CharacterDetail from "./CharacterDetail";
+import logo from "../images/Rick_and_Morty_title_card.png";
 
 function App() {
-    return <h1>Evaluación Final React </h1>;
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <div>
+                        <h1>Evaluación Final React </h1>
+                        <img
+                            src={logo}
+                            alt="Rick and Morty Logo"
+                            className="logo"
+                        />
+                    </div>
+                }
+            />
+            <Route path="/detail/:id" element={<CharacterDetail />} />
+        </Routes>
+    );
 }
 
 export default App;
