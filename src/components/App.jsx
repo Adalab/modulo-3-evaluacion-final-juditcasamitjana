cd77985f769e2c388df94911e3eb5ce85a457932
Judit Casamitjana <juditcasamitjana@gmail.com>
2025-04-04 13:07:43 +0200

    character list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,66 @@
 import { Route, Routes } from "react-router-dom";
-import "../scss/App.scss";
 import CharacterDetail from "./CharacterDetail";
-import RickAndMorty from "./RickAndMorty";
+import { useEffect, useState } from "react";
+import Filters from "./Filters";
+import Header from "./Header";
+import CharacterList from "./CharacterList";
+import "../scss/App.scss";
 
 function App() {
+    const [nameFilter, setNameFilter] = useState("");
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetch("https://rickandmortyapi.com/api/character")
+            .then((data) => data.json())
+            .then((data) => {
+                setCharacters(
+                    data.results.map((result) => {
+                        return {
+                            id: result.id,
+                            image: result.image,
+                            name: result.name,
+                            species: result.species,
+                            status: result.status,
+                        };
+                    })
+                );
+            });
+    }, []);
+
+    const handleNameFilter = (nameFilter) => {
+        setNameFilter(nameFilter);
+    };
+
+    const filteredCharacters = characters.filter((character) => {
+        return character.name
+            .toLowerCase()
+            .includes(nameFilter.toLocaleLowerCase());
+    });
+
     return (
-        <Routes>
-            <Route path="/" element={<RickAndMorty />} />
-            <Route path="/detail/:id" element={<CharacterDetail />} />
-        </Routes>
+        <>
+            <Header />
+            <main>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <Filters onNameFilter={handleNameFilter} />
+                                <CharacterList
+                                    characters={filteredCharacters}
+                                />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/detail/:id"
+                        element={<CharacterDetail characters={characters} />}
+                    />
+                </Routes>
+            </main>
+        </>
     );
 }
 
--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CharacterCard({ character }) {
     return (
         <li>
-            <div>
-                <img src={character.image} alt={character.name} />
-            </div>
-            <p>{character.name}</p>
-            <p>{character.species}</p>
+            <Link to={`/detail/${character.id}`}>
+                <div>
+                    <img src={character.image} alt={character.name} />
+                </div>
+                <p>{character.name}</p>
+                <p>{character.species}</p>
+                <p>{character.status}</p>
+            </Link>
         </li>
     );
 }
